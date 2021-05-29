a18d46d61bf9e4747838ed084307016376df2dd2
Sedrick <digitalphenom0@gmail.com>
2021-05-29 01:44:00 -0700

    setup winning conditions

--- rpsGame.js
+++ rpsGame.js
@@ -8,16 +8,75 @@
 
 function computerPlay(){
 
-let  chosenOption = Math.floor(Math.random()*3)
+let  computerOption = Math.floor(Math.random()*3)
 
-if (chosenOption === 0) {
-    console.log('rock')
+if (computerOption === 0) {
+    return 'rock'
 }
-else if (chosenOption === 1){
-    console.log('paper')
+else if (computerOption === 1){
+    return 'paper'
 }
-else if (chosenOption === 2){
-    console.log('scissors')
+else if (computerOption === 2){
+    return 'scissors'
 }
 }
 
+    
+
+
+
+
+function playRound(playerSelection, computerSelection)
+ {
+// How can I compare two strings to determine the winner?
+if (playerSelection == 'scissors' && computerSelection == 'rock'){
+    return 'Rock beats Scissors You loose!';
+}
+else if (playerSelection == 'rock' && computerSelection == 'scissors'){
+    return 'Rock beats Scissors You Win!!';
+
+}
+ }
+
+
+ const playerSelection = "scissors";
+const computerSelection = computerPlay();
+console.log(playRound('rock', 'scissors'))
+
+
+
+
+// function playRound(playerSelection, computerSelection)
+//     let rock = 1
+//     let paper = 2;
+//     let scissors = 3;
+    
+//     if (rock < paper){
+//         console.log("You loose! Paper beats Rock")
+//     }
+//     else if (paper > rock){
+//         console.log("You Win!! Paper beats Rock")
+//     }
+//     else if (rock == rock){
+//         console.log("Tie Pick again")
+//     }
+
+
+
+// if ('rock' 'paper') {
+   
+//     return 'you loose';
+// }
+// else if (rock > paper) {
+//     console.log('you Win')
+// }
+// else if (rock == rock) {
+//     console.log('Tie')
+// }
+// else if (paper == paper) {
+//     console.log('Tie')
+// }
+
+
+
+// 
