f29d1a0e166bce3db950396cfddc910ce49db668
Sedrick <digitalphenom0@gmail.com>
2021-05-28 21:08:04 -0700

    set_computer_function

---
+++ rpsGame.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+// let options = ['rock','paper','scissors']
+
+
+function computerPlay(){
+
+let  chosenOption = Math.floor(Math.random()*3)
+
+if (chosenOption === 0) {
+    console.log('rock')
+}
+else if (chosenOption === 1){
+    console.log('paper')
+}
+else if (chosenOption === 2){
+    console.log('scissors')
+}
+}
+
